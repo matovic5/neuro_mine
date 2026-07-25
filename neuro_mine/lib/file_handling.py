4c5705ba1b856d9fe5317791eebcb77537d0464c
haesemeyer <m.haesemeyer@gmail.com>
2026-07-25 17:00:59 -0400

    Updated routine for file loading to improve robustness. Now support text data as categories

--- neuro_mine/lib/file_handling.py
+++ neuro_mine/lib/file_handling.py
@@ -79,6 +79,7 @@ class FileParser:
         except (ValueError, OverflowError):
             return None  # neither a float nor a date - should we throw an exception here instead of returning None?
 
+
 class CSVParser(FileParser):
     """
     Parser for CSV files
@@ -90,103 +91,174 @@ class CSVParser(FileParser):
         :param prefix: If no column names are provided in the file this prefix will be used to label columns
         """
         super().__init__(file_path)
-        self.delimiter = CSVParser._find_delimiter(self.filename)
+        self.delimiter = self._find_delimiter()
+        self.col_count = self._validate_file_contents()
+        if self.col_count == -1:
+            raise IOError("CSV file has different column counts across rows. Please ensure that all rows have the same number of columns.")
         self.prefix = prefix
 
-    @staticmethod
-    def _find_delimiter(filename):
+    def _find_delimiter(self):
         sniffer = csv.Sniffer()
-        with open(filename) as fp:
+        with open(self.filename) as fp:
             delimiter = sniffer.sniff(fp.read(-1)).delimiter
         return delimiter
+
+    def _validate_file_contents(self) -> int:
+        """
+        Performs basic validation of file contents to ensure that all rows within the file have the same number of components
+        :return: Number of columns if validated otherwise -1
+        """
+        with open(self.filename, "r") as f:
+            lines = f.readlines()
+        col_count = None
+        for l in lines:
+            if col_count is None:
+                col_count = len(l.strip().split(self.delimiter))
+            else:
+                if col_count != len(l.strip().split(self.delimiter)):
+                    return -1
+        return col_count
+
+    def _load_date_column(self, column: List[str], col_name: int) -> np.ndarray:
+        """
+        Loads a column with encoded dates/times and encodes them as seconds elapsed since the millennium start such that
+        they can be properly related for interpolation across files
+        :param column: The file column
+        :param col_name: The name of the column
+        :return: The date-time-values encoded as seconds since the millennium start
+        """
+        retval = np.full((len(column), 1), np.nan)
+        # In the following we want to recode time into a count of seconds relative to a start time
+        # since we do not know when predictors recording started relative to response acquisition
+        # this start time has to be independent of the timestamp itself. However, it shouldn't be
+        # so far in the past that we lose resolution. Arbitrarily, we chose the millennium
+        # The better way would clearly be to find the minimum of predictor and response start times
+        # and use that value but that can only happen after file matching
+        t0 = datetime.combine(date(2000, 1, 1), time())
+        for i, elem in enumerate(column):
+            # we skip empty cells
+            if elem.strip() == "":
+                continue
+            t = self._parse_datetime(elem)
+            if t is None:
+                raise ValueError(f"Column {col_name} was identified as containing date-time information but contains "
+                                 f"mixed content which is not supported. This can be caused by ambiguous encoding or "
+                                 f"misidentification of a malformed header.")
+            t_seconds = (t - t0).total_seconds()
+            retval[i] = t_seconds
+        return retval
+
+    @staticmethod
+    def _load_string_column(column: List[str], col_name: int) -> np.ndarray:
+        """
+        Loads a column with text and interprets unique strings as categories
+        :param column: The file column
+        :param col_name: The name of the column
+        :return: The categories encoded as integers
+        """
+        retval = np.full((len(column), 1), np.nan)
+        categ_dict = {}
+        categ_count = 0
+        for i, elem in enumerate(column):
+            # we skip empty cells
+            if elem.strip() == "":
+                continue
+            valid = False
+            try:
+                float(elem)
+            except ValueError:
+                valid = True
+            if not valid:
+                raise ValueError(f"Column {col_name} was identified as containing text but contains at least one number."
+                                 f"Mixed content is not supported. This can be caused by ambiguous encoding or "
+                                 f"misidentification of a malformed header.")
+            if elem not in categ_dict:
+                categ_dict[elem] = categ_count
+                categ_count += 1
+            retval[i] = categ_dict[elem]
+        if categ_count > 2:
+            print("###", flush=True)
+            print(f"Column {col_name} was interpreted as categorical data with {categ_count} categories. Processing will"
+                  f" continue, however, categorical inputs are not properly supported.", flush=True)
+            print("###", flush=True)
+        return retval
+
+    @staticmethod
+    def _load_numerical_column(column: List[str], col_name: int) -> np.ndarray:
+        """
+        Loads a column with (floating point) numbers
+        :param column: The file column
+        :param col_name: The name of the column
+        :return: The numerical data as floating point numbers
+        """
+        retval = np.full((len(column), 1), np.nan)
+        for i, elem in enumerate(column):
+            # we skip empty cells
+            if elem.strip() == "":
+                continue
+            try:
+                value = float(elem)
+            except ValueError:
+                raise ValueError(
+                    f"Column {col_name} was identified as containing numerical data but contains at least one text item."
+                    f"Mixed content is not supported. This can be caused by ambiguous encoding or "
+                    f"misidentification of a malformed header.")
+            retval[i] = value
+        return retval
 
     def load_data(self) -> Tuple[np.ndarray, bool, List]:
         """
         Loads the data from the file
         :return:
         """
+        # We allow at most one header row - so we simply ask csv sniffer if there is a header or not
+        sniffer = csv.Sniffer()
+        with open(self.filename) as f:
+            has_header = sniffer.has_header(f.read(-1))
+
         # Load as text-file, processing line-by-line
         with open(self.filename, "r") as f:
             lines = f.readlines()
 
-        # Identify number of header rows - these can be rows that contain actual column headers
-        # or rows with descriptive text. In other words we try to find the first true data-row here
-        # using simple heuristics which will likely break if the user supplies pathological files
-        skip = 0  # counter of how many rows to skip as header
-        ncols = None  # the number of actual data columns
-        header_row = None  # the column labels
-
-        # Detect where numeric data starts
-        for line in lines:
-            parts = line.strip().split(self.delimiter)
-            # the first column has to contain time, however this can be either floating points
-            # counting seconds/minutes/etc or actual time objects
-            first_col = self._parse_datetime(parts[0])
-
-            if first_col is not None:  # datetime in first column
-                try:
-                    [float(x) for x in parts[1:] if x != ""]
-                    ncols = len(parts)
-                    break
-                except ValueError:
-                    skip += 1  # the row contained a non-numerical object and is therefore considered a header
-            else:  # try full row as floats, since time should be numerical
-                try:
-                    [float(x) for x in parts if x != ""]
-                    ncols = len(parts)
-                    break
-                except ValueError:
-                    skip += 1
-
-        # Try to find column header definition in skipped rows - this should be a row
-        # that has the same number of columns as the data
-        assert ncols is not None
-        if skip > 0:
-            for i in range(skip - 1, -1, -1):
-                parts = lines[i].strip().split(self.delimiter)
-                if len(parts) == ncols:
-                    header_row = parts
-                    break
-
-        # assemble all lines that have a full column count
-        numeric_part = [line.strip().split(self.delimiter) for line in lines[skip:]]
-        numeric_part = [row for row in numeric_part if len(row) == ncols]
-
-        data = []
-        # determine how time is saved within the file
-        first_val = self._parse_datetime(numeric_part[0][0])
-        first_is_datetime = first_val is not None
-
-        if first_is_datetime:
-            # In the following we want to recode time into a count of seconds relative to a start time
-            # since we do not know when predictors recording started relative to response acquisition
-            # this start time has to be independent of the timestamp itself. However, it shouldn't be
-            # so far in the past that we lose resolution. Arbitrarily, we chose the millennium
-            # The better way would clearly be to find the minimum of predictor and response start times
-            # and use that value but that can only happen after file matching
-            t0 = datetime.combine(date(2000, 1, 1), time())
-            for row in numeric_part:
-                t = self._parse_datetime(row[0])
-                ts = (t - t0).total_seconds()
-                rest = [float(x) if x != "" else np.nan for x in row[1:]]
-                data.append([ts] + rest)
+        # Determine data header (column names)
+        if has_header:
+            data_header = lines[0].strip().split(self.delimiter)
+            skip = 1  # skip first row when loading data
         else:
-            for row in numeric_part:
-                data.append([float(x) if x != "" else np.nan for x in row])
+            # The name of the first column is Time
+            data_header = ["Time"]+[f"{self.prefix}_{i}" for i in range(self.col_count-1)]
+            skip = 0
 
-        data = np.array(data, dtype=float)
-        data_has_header = header_row is not None
+        # assemble all lines that contain data, split by the delimiter
+        file_contents = [line.strip().split(self.delimiter) for line in lines[skip:]]
+
+        # reorganize data into individual columns
+        file_columns = []
+        for i in range(self.col_count):
+            file_columns.append([d[i] for d in file_contents])
+
+        # assemble converted numerical data
+        data_columns = []
+        for i, dc in enumerate(file_columns):
+            # determine if column contains: (a) a datetime, (b) a float, (c) a string
+            t = self._parse_datetime(dc[0])
+            if t is not None:
+                data_columns.append(self._load_date_column(dc, i))
+                continue
+            try:
+                float(dc[0])
+            except ValueError:
+                data_columns.append(self._load_string_column(dc, i))
+                continue
+            data_columns.append(self._load_numerical_column(dc, i))
+
+
+        data = np.hstack(data_columns)
 
         # Make sure that the first column can be interpreted as time
         if any(np.diff(data[:, 0]) <= 0):
-            raise ValueError("First column in datafile should be time, but values aren't strictly increasing")
-
-        if data_has_header:
-            data_header = header_row
-        else:
-            ncols = data.shape[1]
-            # The name of the first column is Time
-            data_header = ["Time"]+[f"{self.prefix}_{i}" for i in range(ncols-1)]
+            raise ValueError("The first column in each datafile should be time. However values aren't strictly increasing,"
+                             " which means that data cannot be assigned to unique timepoints.")
 
         # remove columns that contain all NaN - these can occur when cell contents in CSV files
         # are deleted without deleting the column itself
@@ -202,4 +274,4 @@ class CSVParser(FileParser):
             print("###", flush=True)
         data = data[np.logical_not(has_nan)]
 
-        return data, data_has_header, data_header
+        return data, has_header, data_header
